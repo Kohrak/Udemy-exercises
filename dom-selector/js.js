9bfb32009b953be05d6466f8b7d7802b1ebb62a8
Kohrak <gabrielgt95@gmail.com>
2018-02-17 18:37:08 +0000

    complete selector exercise

---
+++ dom-selector/js.js
@@ -0,0 +1,9 @@
+function selec(selectorx, color){
+  selectorx.style.color = color;
+}
+
+var first = document.getElementById("first");
+var second = document.getElementsByTagName("p")[0];
+var third = document.getElementsByClassName("special")[0];
+var fourth = document.querySelector("#first")
+var fifth = document.querySelector(".special")
